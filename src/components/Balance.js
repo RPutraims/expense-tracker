4a3fef781ae09cecd43d7dd6388aac274f242c4b
RalfsPutraims <161470810+RPutraims@users.noreply.github.com>
2025-10-06 18:42:23 +0300

    initialise UI

---
+++ src/components/Balance.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import App from '../App.css';
+
+const Header = () => {
+  return (
+    <>
+        <h4>Your Balance</h4>
+        <h1>0.00€</h1>
+    </>
+  )
+}
+
+export default Header
